42c6e428ddb155da3a8a4512df4f2f0916575b5a
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-02-18 12:50:24 -0800

    Update get_PDB_info_paralell_submit.sh

--- get_PDB_info_paralell_submit.sh
+++ get_PDB_info_paralell_submit.sh
@@ -4,6 +4,13 @@
 #$ -R yes
 #$ -V
 
+#Stephanie Wankowicz
+#https://stephaniewankowicz.github.io/
+#Fraser Lab UCSF
+
+#This script takes in a list of PDB IDs. For each PDB, it will spit out its own processes, which will run get_PDB_info_paralell.sh.
+#The folder structures for the PDBs will be /base_folder/PDB_ID/.
+
 #________________________________________________INPUTS________________________________________________
 PDB_file=PDB_2A_res.txt
 base_folder='/location/you/would/like/folders/of/PDBs/to/exist/'
@@ -16,4 +23,4 @@ source activate qfit
 #________________________________________________RUN SCRIPT____________________________________________
 PDB=$(cat $PDB_file | head -n $SGE_TASK_ID | tail -n 1)
 
-sh get_PDB_info ${PDB} ${base_folder}
+sh get_PDB_info_paralell.sh ${PDB} ${base_folder}
