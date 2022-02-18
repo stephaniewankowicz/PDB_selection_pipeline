94fe7c3129ba1479bda6f4c53af361c5440df3a1
Stephanie (Mullane) Wankowicz <stephanie.wankowicz@ucsf.edu>
2022-02-18 12:41:24 -0800

    Delete gather_seq.sh

--- gather_seq.sh
+++
@@ -1,8 +0,0 @@
-file=/wynton/group/fraser/swankowicz/holo.txt
-for line in $(cat $file); do
-   echo $line
-   SEQ1=$(python /wynton/group/fraser/swankowicz/selecting_apo_holo/get_seq.py /wynton/group/fraser/swankowicz/AWS_refine_done/${line}/${line}.pdb)
-   echo $SEQ1
-   echo "> ${line}" >> /wynton/group/fraser/swankowicz/sequences_holo.txt
-   echo $SEQ1 >> /wynton/group/fraser/swankowicz/sequences_holo.txt
-done
